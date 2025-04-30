2811eb19ee1507f4ac5f15695fb435138ccbc3ef
Vipul Tyagi <vipultyagiabes@gmail.com>
2025-04-30 10:03:23 +0530

    LINKED LIST:: L8_ReverseDLL

---
+++ coding/src/topics/linkedList/doublyLinkedList/L8_ReverseDLL.java
@@ -0,0 +1,86 @@
+package topics.linkedList.doublyLinkedList;
+
+public class L8_ReverseDLL {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList dll = new DoublyLinkedList();
+        dll.push(10);
+        dll.push(20);
+        dll.push(30);
+        dll.push(40);
+
+        System.out.println("Original list:");
+        dll.printList();
+
+        dll.reverse();
+
+        System.out.println("Reversed list:");
+        dll.printList();
+
+    }
+
+    private static Node reverseTheDLL(Node head) {
+        Node current = head;
+        Node newHead = null;
+
+        while (current != null) {
+            // Swap prev and next
+            Node temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+
+            // Set newHead to current node (will eventually be the last node processed)
+            newHead = current;
+
+            // Move to next node (which is prev due to the swap)
+            current = current.prev;
+        }
+
+        return newHead;
+    }
+
+
+}
+
+class DoublyLinkedList {
+    Node head;
+
+    // Function to reverse the doubly linked list
+    void reverse() {
+        Node current = head;
+        Node temp = null;
+
+        while (current != null) {
+            // Swap next and prev
+            temp = current.prev;
+            current.prev = current.next;
+            current.next = temp;
+
+            // Move to next node (which is previous before swap)
+            current = current.prev;
+        }
+
+        // After the loop, temp is at the new head
+        if (temp != null) {
+            head = temp.prev;
+        }
+    }
+
+    // Helper methods to add nodes and print list
+    void push(int newData) {
+        Node newNode = new Node(newData);
+        newNode.next = head;
+        if (head != null) head.prev = newNode;
+        head = newNode;
+    }
+
+    void printList() {
+        Node node = head;
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
